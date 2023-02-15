2065a2c1cc30d44f5eaea9fa9aba648c45c8a20b
Matthias <Matthias>
2023-02-15 02:52:25 +0100

    implemented finalize and return traits

--- neo4j_cypher/src/query/merge_query.rs
+++ neo4j_cypher/src/query/merge_query.rs
@@ -1,3 +1,6 @@
+use crate::query::finalize::FinalizeTrait;
+use crate::query::return_query::{ReturnParamTrait, ReturnQuery, ReturnTrait};
+
 pub struct MergeQuery {
     nv: String,
     state: String,
@@ -6,6 +9,26 @@ pub struct MergeQuery {
 impl MergeQuery {
     pub fn new(nv: String, state: String) -> Self {
         MergeQuery { nv, state }
+    }
+}
+
+impl FinalizeTrait for MergeQuery {
+    fn finalize(&self) -> String {
+        self.state.clone()
+    }
+}
+
+impl ReturnTrait for MergeQuery {
+    fn r#return(&mut self, nv: &str) -> Box<dyn ReturnParamTrait> {
+        super::return_query::return_method(&self.state, vec![nv], None)
+    }
+
+    fn return_field(&mut self, nv: &str, field: &str) -> Box<dyn ReturnParamTrait> {
+        super::return_query::return_method(&self.state, vec![nv], Some(field))
+    }
+
+    fn return_many(&mut self, nvs: Vec<&str>) -> Box<dyn ReturnParamTrait> {
+        super::return_query::return_method(&self.state, nvs, None)
     }
 }
 
